6e0cc075b3c4bb945665183712782eeba89a823d
Jeysi/ <119491303+JcFaderanga@users.noreply.github.com>
2023-02-13 00:28:10 +0800

    Add files via upload

---
+++ C-activity.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    
+char first_name[20], surname[20],year[20], motto[50];
+int bd;
+
+printf("Enter first name: ");
+scanf("%s", &first_name);
+printf("Enter surname name: ");
+scanf("%s", &surname_name);
+printf("Enter year and section: ");
+scanf("%s", &year);
+printf("Enter Full Birth day: ");
+scanf("%d", &bd);
+printf("Enter motto: ");
+scanf("%s", &motto);
+
+printf("Hi! I'm %s %s of %s Welcome to Data Structures and Algorithm! My Birthday is on %d and my motto/motivation in life is %s \n" ,name,yr,bd,motto);
+    return 0;
+}
